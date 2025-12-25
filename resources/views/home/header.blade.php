542f54f31fc597bd36ea5381986666e378484df7
Athaurrahman11 <atha278200@gmail.com>
2025-12-25 21:44:59 +0530

    created responsive hero text and styled

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,17 +1,17 @@
- <header class="bg-amber-950 flex justify-between items-center p-6 pr-28 pl-28 shadow-2xl relative z-50">
+ <header class="bg-pink-950 flex justify-between items-center p-6 pr-28 pl-28 shadow-2xl relative z-50">
       <div id="title">
         <h1 class="text-green-500 font-bold text-2xl">FABRIO DINE</h1>
       </div>
 
       <!-- Navigation -->
-      <nav id="menu" class="fixed top-0 left-0 h-screen md:h-0 w-64  md:w-fit bg-amber-950 text-white transform -translate-x-full md:translate-x-0 md:static md:flex md:flex-row md:justify-between md:items-center md:space-x-5 p-11 md:p-0 transition-transform duration-150 ease-linear z-5">
+      <nav id="menu" class="fixed top-0 left-0 h-screen md:h-0 w-64  md:w-fit bg-pink-950 text-white transform -translate-x-full md:translate-x-0 md:static md:flex md:flex-row md:justify-between md:items-center md:space-x-8 p-11 md:p-0 transition-transform duration-150 ease-linear z-5">
         <a href="#"><i class="hover:text-green-500 transition duration-150 ri-close-line md:hidden absolute right-7 top-7" id="close-btn"></i></a>
-        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-[''] after:w-4/5 after:h-[1.5px] after:bg-green-500 after:block after:absolute after:left-1/2 after:bottom-0 after:translate-x-[-50%] after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" id="home">Home</a>
-        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-[''] after:w-4/5 after:h-[1.5px] after:bg-green-500 after:block after:absolute after:left-1/2 after:bottom-0 after:translate-x-[-50%] after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100">Menu</a>
-        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-[''] after:w-4/5 after:h-[1.5px] after:bg-green-500 after:block after:absolute after:left-1/2 after:bottom-0 after:translate-x-[-50%] after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >Shop</a>
-        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-[''] after:w-4/5 after:h-[1.5px] after:bg-green-500 after:block after:absolute after:left-1/2 after:bottom-0 after:translate-x-[-50%] after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >About</a>
-        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-[''] after:w-4/5 after:h-[1.5px] after:bg-green-500 after:block after:absolute after:left-1/2 after:bottom-0 after:translate-x-[-50%] after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >Our Team</a>
-        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-[''] after:w-4/5 after:h-[1.5px] after:bg-green-500 after:block after:absolute after:left-1/2 after:bottom-0 after:translate-x-[-50%] after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >Contact</a>
+        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-['']  after:h-[1.5px] after:bg-green-500 after:block after:w-auto after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" id="home">Home</a>
+        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-['']  after:h-[1.5px] after:bg-green-500 after:block after:w-auto after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100">Menu</a>
+        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-['']  after:h-[1.5px] after:bg-green-500 after:block after:w-auto after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >Shop</a>
+        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-['']  after:h-[1.5px] after:bg-green-500 after:block after:w-auto after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >About</a>
+        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-['']  after:h-[1.5px] after:bg-green-500 after:block after:w-auto after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >Our Team</a>
+        <a href="#" class="block mt-6 md:mt-0 hover:text-green-500 transition duration-150 relative after:content-['']  after:h-[1.5px] after:bg-green-500 after:block after:w-auto after:opacity-0 after:transition after:duration-150 after:ease-in-out hover:after:opacity-100" >Contact</a>
       </nav>
 
       <!-- Icons -->
