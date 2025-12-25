65c0e30f2abb900f7ee457b4019b12d9c9d53610
Athaurrahman11 <atha278200@gmail.com>
2025-12-25 20:55:43 +0530

    changed the text size in the hero

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -1,7 +1,7 @@
 <section class="hero md:bg-center md:bg-no-repeat  md:bg-cover bg-cover bg-right bg-no-repeat flex justify-center items-center  ">
   <div class="text-center  flex flex-col items-center justify-center p-15">
     <h1 class="text-white text-7xl font-semibold ">Welcome to <span class="text-green-500 font-bold">Fabrio Dine</span></h1>
-    <p class="text-white text-xl text-semibold mt-10">Discover a world of mouthwatering dishes made with love and passion. Every bite tells a story of flavors, crafted just for you</p>
+    <p class="text-white text-3xl md:text-4xl text-semibold mt-10">Discover a world of mouthwatering dishes made with love and passion. Every bite tells a story of flavors, crafted just for you</p>
     <a href="" class="bg-amber-950 p-3  pl-9 pr-9 rounded-3xl mt-10 text-[#d2b475] font-[600]">ORDER NOW</a>
   </div>
 </section>
